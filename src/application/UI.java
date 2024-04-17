4f89601bbc1930da8128e7580d601db9f6fad7b4
Naeff Matheus <naeffm7@hotmai.com>
2024-04-17 20:41:00 -0300

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -58,9 +58,16 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
-		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-		if (chessMatch.getCheck()) {
-			System.out.println("CHECK!");
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+			}
+		}
+		else { 
+			System.out.println("CHECKMATE!");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
+		
 		}
 	}
 
@@ -88,7 +95,7 @@ public class UI {
 
 		System.out.println("  a b c d e f g h");
 
-	}
+	} 
 	
 	private static void printPiece(ChessPiece piece, boolean background) {
 		if(background) {
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -17,7 +17,7 @@ public abstract class ChessPiece extends Piece{
 		return color;
 	}
 	
-	public ChessPosition getCHessPosition() {
+	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
 	}
 	
